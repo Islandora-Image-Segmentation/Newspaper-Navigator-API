c98dba8e57fbae8100d431a7a4aa6dec25b350c6
Kosay Jabre <kosay@thinkingbig.net>
2021-01-28 16:36:28 -0400

    Fixed issue where config was not being found

--- model.py
+++ model.py
@@ -4,16 +4,18 @@
 import os
 
 import detectron2
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
 
 
 SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 RESOURCES_DIRECTORY = os.path.join(SCRIPT_DIRECTORY, "resources")
 MODEL_WEIGHTS_PATH = os.path.join(RESOURCES_DIRECTORY, "model_final.pth")
-MODEL_CONFIG_PATH = os.path.join(RESOURCES_DIRECTORY, "faster_rcnn_R_50_FPN_3x.yaml")
 _model = None
 
 
 def get_model():
+    global _model
     if _model is None:
         _model = load_model_from_disk()
     return _model
@@ -22,8 +24,8 @@ def get_model():
 def load_model_from_disk():
     if os.path.exists(MODEL_WEIGHTS_PATH):
         model_config = build_detectron_config()
-        model = detectron2.build_model(model_config)
-        detectron2.DetectionCheckpointer(model).load(MODEL_WEIGHTS_PATH)
+        model = detectron2.modeling.build_model(model_config)
+        detectron2.checkpoint.DetectionCheckpointer(model).load(MODEL_WEIGHTS_PATH)
         model.train(False)
         return model
     else:
@@ -31,10 +33,8 @@ def load_model_from_disk():
 
 
 def build_detectron_config():
-    if os.path.exists(MODEL_CONFIG_PATH):
-        model_config = detectron2.get_cfg()
-        model_config.merge_from_file(MODEL_CONFIG_PATH)
-        model_config.MODEL.ROI_HEADS.NUM_CLASSES = 7
-        return model_config
-    else:
-        raise Exception(f"Could not find the model config at {MODEL_CONFIG_PATH}. Please follow the installation instructions to download the config.")
+    model_config = get_cfg()
+    model_config.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+    model_config.MODEL.ROI_HEADS.NUM_CLASSES = 7
+    model_config.MODEL.DEVICE = 'cpu'
+    return model_config
