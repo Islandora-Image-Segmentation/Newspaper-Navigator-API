db802830fcf8f9320efd338eac9a93d54f7cf6e0
dynac01 <robinolisa@rocketmail.com>
2021-01-30 16:35:25 -0400

    Minor formatting change

--- ocr.py
+++ ocr.py
@@ -1,10 +1,7 @@
 from PIL import Image
-import pytesseract
 from pytesseract import image_to_string
 
-def retrieve_ocr(image: Image.Image):
-  # Tell pytesseract where it's stored on windows devices, not necessary for linux or unix based systems
-  pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 
-  # Extract string from image using pytesseract
+def retrieve_ocr(image: Image.Image) -> str:
+  """ Extract string from image using pytesseract """
   return image_to_string(image)
