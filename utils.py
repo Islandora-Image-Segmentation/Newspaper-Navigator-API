cb1ce6dc471c3cd3a005b47a21e05fd445f7d5a6
Kosay Jabre <kosay@thinkingbig.net>
2021-02-01 14:21:48 -0400

    Simplified crop function

--- utils.py
+++ utils.py
@@ -5,9 +5,8 @@ import config
 
 def crop(image: Image.Image, box) -> Image.Image:
   """ Use built in crop function for PIL Image. Normalized Box co-ordinates converted to image co-ordinates. """
-  cropped = image.crop((box[0] * image.width, box[1] * image.height, box[2] * image.width, box[3] * image.height))
+  return image.crop((box[0] * image.width, box[1] * image.height, box[2] * image.width, box[3] * image.height))
 
-  return cropped
 
 
 def standardize_image(image: Image.Image) -> Image.Image:
