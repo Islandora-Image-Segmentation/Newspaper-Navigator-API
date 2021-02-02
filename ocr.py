6d318e22a8ee08c3fd707c7f878e65f34b9cef7f
dynac01 <robinolisa@rocketmail.com>
2021-02-02 00:40:25 -0400

    retrieve_ocr function returns HOCR as well

--- ocr.py
+++ ocr.py
@@ -1,7 +1,7 @@
 from PIL import Image
-from pytesseract import image_to_string
+from pytesseract import image_to_string, image_to_pdf_or_hocr
 
 
-def retrieve_ocr(image: Image.Image) -> str:
-  """ Extract string from image using pytesseract """
-  return image_to_string(image)
+def retrieve_ocr(image: Image.Image) -> dict:
+  """ Extract string and HOCR from image using pytesseract """
+  return {"string": image_to_string(image), "hocr": image_to_pdf_or_hocr(image, extension='hocr')}
