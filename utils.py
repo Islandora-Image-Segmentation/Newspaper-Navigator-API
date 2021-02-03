f6eddffc987cbeb26116b6b96134253ee2a2c99c
Kosay Jabre <kosay@thinkingbig.net>
2021-02-03 16:58:15 -0400

    Added an image_format kwarg to image_to_base64

--- utils.py
+++ utils.py
@@ -7,9 +7,8 @@ import config
 
 
 def crop(image: Image.Image, box) -> Image.Image:
-  """ Use built in crop function for PIL Image. Normalized Box co-ordinates converted to image co-ordinates. """
+  """Crop the image according to box, which should contains normalized coordinates."""
   return image.crop((box[0] * image.width, box[1] * image.height, box[2] * image.width, box[3] * image.height))
-
 
 
 def standardize_image(image: Image.Image) -> Image.Image:
@@ -19,18 +18,11 @@ def standardize_image(image: Image.Image) -> Image.Image:
       standardized_image.thumbnail((config.MAX_IMAGE_SIZE, config.MAX_IMAGE_SIZE), Image.ANTIALIAS)
   return standardized_image
 
-  
-  
 
-def crop(image: Image.Image, box) -> Image.Image:
-  """Crop the image according to box, which should contains normalized coordinates."""
-  return image.crop((box[0] * image.width, box[1] * image.height, box[2] * image.width, box[3] * image.height))
-
-
-def image_to_base64(image: Image.Image) -> str:
+def image_to_base64(image: Image.Image, image_format="JPEG2000") -> str:
   memory_buffer = io.BytesIO()
-  image.save(memory_buffer, format='PNG')
-  return base64.b64encode(memory_buffer.getvalue()).decode('ascii')
+  image.save(memory_buffer, format=image_format)
+  return base64.b64encode(memory_buffer.getvalue()).decode("ascii")
 
 
 def base64_to_image(base64_string: str) -> Image.Image:
