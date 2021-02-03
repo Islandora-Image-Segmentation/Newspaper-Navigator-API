f9d329f2421dc2e9ac3cede1c74145f310409d4a
Kosay Jabre <kosay@thinkingbig.net>
2021-02-03 14:50:53 -0400

    Added image_to_base64 and base64_to_image functions

--- utils.py
+++ utils.py
@@ -1,7 +1,20 @@
+import io
+import base64
+
 from PIL import Image
 
-def crop(image: Image.Image, box) -> Image.Image:
-  """ Use built in crop function for PIL Image. Normalized Box co-ordinates converted to image co-ordinates. """
-  cropped = image.crop((box[0] * image.width, box[1] * image.height, box[2] * image.width, box[3] * image.height))
 
-  return cropped
+def crop(image: Image.Image, box) -> Image.Image:
+  """Crop the image according to box, which should contains normalized coordinates."""
+  return image.crop((box[0] * image.width, box[1] * image.height, box[2] * image.width, box[3] * image.height))
+
+
+def image_to_base64(image: Image.Image) -> str:
+  memory_buffer = io.BytesIO()
+  image.save(memory_buffer, format='PNG')
+  return base64.b64encode(memory_buffer.getvalue()).decode('ascii')
+
+
+def base64_to_image(base64_string: str) -> Image.Image:
+  image_bytes = base64.b64decode(base64_string)
+  return Image.open(io.BytesiIO(image_bytes))
