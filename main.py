7b8fb91dbb71479363470e855d2b611d3aa58bf7
Kosay Jabre <kosay@thinkingbig.net>
2021-02-05 16:34:27 -0400

    Implemented segment_article pipeline

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from fastapi import FastAPI
 
 import utils
 import inference
-from schemas import SegmentationRequest
-from schemas import SegmentationResponse
-from schemas import Article
-
+import ocr
+import embedding
+import schemas
 
 
 parser = argparse.ArgumentParser()
@@ -22,11 +21,33 @@ app = FastAPI()
 
 
 @app.post("/api/segment_article")
-async def segment_article(request: SegmentationRequest) -> SegmentationResponse:
-    image = utils.base64_to_image(request.image_base64)
-    standardized_image = utils.standardize_image(image)
-    return inference.predict(standardized_image)
-  
+async def segment_article(request: schemas.SegmentationRequest) -> schemas.SegmentationResponse:
+    try:
+        image = utils.base64_to_image(request.image_base64)
+        standardized_image = utils.standardize_image(image)
+        model_output = inference.predict(standardized_image)
+        segment_images = [utils.crop(image, box) for box in model_output.bounding_boxes]
+        segment_ocr = [ocr.retrieve_ocr(image) for image in segment_images]
+        segment_embeddings = [embedding.generate_embeddings(image) for image in segment_images]
+        
+        segments = []
+        for i in range(len(model_output.bounding_boxes)):
+            segment = schemas.ExtractedSegment(ocr_text=segment_ocr[i],
+                                    bounding_box=model_output.bounding_boxes[i],
+                                    embedding=segment_embeddings[i],
+                                    classification=model_output.classes[i],
+                                    confidence=model_output.confidences[i])
+            segments.append(segment)
+
+        return schemas.SegmentationResponse(status_code=0,
+                                    error_message="",
+                                    segment_count=len(segments),
+                                    segments=segments)
+    except Exception as e:
+        return schemas.SegmentationResponse(status_code=-1,
+                                    error_message=f"Failed to process request due to {repr(e)}",
+                                    segment_count=None,
+                                    segments=None)     
 
 uvicorn.run(app, 
             port=args.port, 
