93d45badc7bc5369a634bff3c19d7604cc5614a3
Kosay Jabre <kosay@thinkingbig.net>
2021-02-07 13:42:51 -0400

    Removed redundant standardization step in inference

--- inference.py
+++ inference.py
@@ -19,8 +19,7 @@ CATEGORIES = {1:"Photograph",
 
 
 def image_to_model_input(image: PIL.Image.Image) -> Dict[str, torch.Tensor]:
-    standardized_image = utils.standardize_image(image)
-    image_array = np.asarray(standardized_image)
+    image_array = np.asarray(image)
     image_array = np.transpose(image_array, (2, 0, 1))
     return {"image": torch.from_numpy(image_array.copy())}
 
@@ -32,12 +31,10 @@ def predict(image: PIL.Image.Image):
     with torch.no_grad():
         model_input = image_to_model_input(image)
         model_output = model([model_input])[0]
-        
         bounding_boxes = model_output["instances"].get_fields()["pred_boxes"].to("cpu").tensor.tolist()
         confidences = model_output["instances"].get_fields()["scores"].to("cpu").tolist()
         classes = model_output["instances"].get_fields()["pred_classes"].to("cpu").tolist()
         classes = [CATEGORIES[num] for num in classes]
-
         normalized_bounding_boxes = []
         for box in bounding_boxes:
             normalized_box = (box[0]/float(width), box[1]/float(height), box[2]/float(width), box[3]/float(height))
