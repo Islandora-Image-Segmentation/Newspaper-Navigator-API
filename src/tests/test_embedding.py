dbfb1b0ad50c7cf08d6ed38da47fc16ef2bbc092
dynac01 <robinolisa@rocketmail.com>
2021-02-09 17:31:12 -0400

    Parametrized existing tests

--- src/tests/test_embedding.py
+++ src/tests/test_embedding.py
@@ -1,3 +1,4 @@
+import pytest
 from embedding import generate_embeddings
 from PIL import Image
 import os
@@ -7,26 +8,15 @@ CURRENT_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 # Absolute path to test_assets
 TEST_ASSETS = os.path.join(CURRENT_SCRIPT_DIR, "test_assets")
 
-"""First embedding function test"""
+"""Embedding function test"""
 
-def test_embedding_one():
-  #Load first Image
-  img = Image.open(os.path.join(TEST_ASSETS, "test_image_one.png")).convert('RGB')
+@pytest.mark.parametrize("test_image", ["test_image_one.png", "test_image_two.png"])
+def test_embedding(test_image):
+  #Load Image
+  img = Image.open(os.path.join(TEST_ASSETS, test_image)).convert('RGB')
 
   #Call generate_embeddings from embedding.py
   embedding = generate_embeddings(img)
 
   #Check to see if function returns non-empty list
-  assert len(embedding) > 0
-
-"""Second embedding function test"""
-
-def test_embedding_two():
-  #Load first Image
-  img = Image.open(os.path.join(TEST_ASSETS, "test_image_two.png")).convert('RGB')
-
-  #Call generate_embeddings from embedding.py
-  embedding = generate_embeddings(img)
-
-  # Check to see if function returns non-empty list
   assert len(embedding) > 0
