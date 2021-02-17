f5b947b2b0483c9719f4e88620c44ca8226ebe48
Kosay Jabre <kosay@thinkingbig.net>
2021-02-17 11:10:10 -0400

    Fixed some residual files after big merge

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-S3_SAVE_DIR = os.path.join(SCRIPT_DIR, "chronam_files")
-OUTPUT_SAVE_DIR = os.path.join(SCRIPT_DIR, "chronam_output")
--- utils.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-def crop(image: Image.Image, box) -> Image.Image:
-  """ Use built in crop function for PIL Image. Normalized Box co-ordinates converted to image co-ordinates. """
-  cropped = image.crop((box[0] * image.width, box[1] * image.height, box[2] * image.width, box[3] * image.height))
-
-  return cropped
