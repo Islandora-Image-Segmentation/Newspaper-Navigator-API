0119bfcdb706899ee6825507ed32ddca6d0bba31
Kosay Jabre <kosay@thinkingbig.net>
2021-03-05 14:55:11 -0400

    Added tests for formdata endpoint

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -15,7 +15,7 @@ client = TestClient(app)
 def test_base64_gibberish():
     base64 = "this is not valid base64"
     response = client.post("/api/segment_base64", json={"image_base64": base64})
-    assert response.json()["status_code"] == -1
+    assert response.json()["status_code"] != 0
     assert response.json()["segment_count"] is None
 
 
@@ -38,7 +38,7 @@ def test_base64_newspaper():
 def test_url_invalid():
     url = "this is not a valid url"
     response = client.post("/api/segment_url", json={"image_url": url})
-    assert response.json()["status_code"] == -1
+    assert response.json()["status_code"] != 0
     assert response.json()["segment_count"] is None
 
 
@@ -54,3 +54,27 @@ def test_url_newspaper():
     response = client.post("/api/segment_url", json={"image_url": url})
     assert response.json()["status_code"] == 0
     assert response.json()["segment_count"] > 0
+
+
+def test_formdata_invalid_file():
+    invalid_file = {'image_file': open('__init__.py', 'rb')}
+    response = client.post("/api/segment_formdata", files=invalid_file)
+    assert response.json()["status_code"] != 0
+    assert response.json()["segment_count"] is None
+
+
+def test_formdata_shapes():
+    shapes_path = os.path.join(TEST_ASSETS_DIR, "shapes.png")
+    image_file = {'image_file': open(shapes_path, 'rb')}
+    response = client.post("/api/segment_formdata", files=image_file)
+    assert response.json()["status_code"] == 0
+    assert response.json()["segment_count"] == 0 
+
+
+
+def test_formdata_newspaper():
+    shapes_path = os.path.join(TEST_ASSETS_DIR, "newspaper_issue.png")
+    image_file = {'image_file': open(shapes_path, 'rb')}
+    response = client.post("/api/segment_formdata", files=image_file)
+    assert response.json()["status_code"] == 0
+    assert response.json()["segment_count"] > 0 
