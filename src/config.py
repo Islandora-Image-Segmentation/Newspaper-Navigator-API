f467b1ea93e25975eccc09e554d8c650bb3d6efe
Kosay Jabre <kosay@thinkingbig.net>
2021-03-26 10:50:40 -0300

    Moved USE_CPU config

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 import os
 import re
 
+USE_CPU = True
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 RESOURCES_DIR = os.path.join(SCRIPT_DIR, "resources")
-USE_CPU = True
 MAX_IMAGE_SIZE = 3840
 MINIMUM_CONFIDENCE_THRESHOLD = 0.666
 IMAGE_DOWNLOAD_TIMEOUT = 5
